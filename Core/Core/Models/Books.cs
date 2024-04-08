ac3547a47fc904433229693dd08cba7372b2a3ea
lalagulaxmed96 <tu6g4w77k@code.edu.az>
2024-04-08 11:52:16 +0400

    task

---
+++ Core/Core/Models/Books.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    internal class Books
+    {
+        public int No{ get; set; }
+        public string Name {  get; set; }
+        public string AuthorName {  get; set; }
+        public int PageCount {  get; set; }
+        public Books(int no, string name, string authorName, int pageCount)
+        {
+            No = no;
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+        }
+    }
+}
